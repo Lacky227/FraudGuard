9dfca3f131ee79c0326d97c761e613189e1ecc6a
Lacky227 <tishko.artyr@gmail.com>
2025-09-25 23:14:00 +0300

    feat: modified FraudDetectionMain to call loadData

--- FraudGuardModel/src/main/java/org/example/FraudDetectionMain.java
+++ FraudGuardModel/src/main/java/org/example/FraudDetectionMain.java
@@ -1,5 +1,8 @@
 package org.example;
 
 public class FraudDetectionMain {
-
+    public static void main(String[] args) {
+        DataProcessor dataProcessor = new DataProcessor();
+        dataProcessor.loadData("/home/artur/IdeaProjects/FraudGuard/FraudGuardModel/src/main/resources/creditcard.csv");
+    }
 }
