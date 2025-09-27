59bd9d32b169297ea4b65040727d758ae6e15769
Lacky227 <tishko.artyr@gmail.com>
2025-09-27 22:16:50 +0300

    feat: update FraudDetectionMain to use SplitData from loadData and add logging

--- FraudGuardModel/src/main/java/org/example/FraudDetectionMain.java
+++ FraudGuardModel/src/main/java/org/example/FraudDetectionMain.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import lombok.extern.slf4j.Slf4j;
+import smile.data.DataFrame;
 
 @Slf4j
 public class FraudDetectionMain {
@@ -9,6 +10,12 @@ public class FraudDetectionMain {
     public static void main(String[] args) {
         log.info("Loading and processing data...");
         DataProcessor dataProcessor = new DataProcessor();
-        dataProcessor.loadData(DATASET_PATH);
+        var processedData = dataProcessor.loadData(DATASET_PATH);
+
+        DataFrame trainData = processedData.trainData();
+        DataFrame testData = processedData.testData();
+
+        log.info("Size training data: rows ({})", trainData.size());
+        log.info("Size testing data: rows ({})", testData.size());
     }
 }
