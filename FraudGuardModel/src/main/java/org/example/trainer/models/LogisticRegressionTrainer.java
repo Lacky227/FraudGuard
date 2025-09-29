028d41d64ea71693875e7fd8bb239106c68facb5
Lacky227 <tishko.artyr@gmail.com>
2025-09-29 23:50:24 +0300

    feat: add classes models and implements from interface ModelTrainer

---
+++ FraudGuardModel/src/main/java/org/example/trainer/models/LogisticRegressionTrainer.java
@@ -0,0 +1,18 @@
+package org.example.trainer.models;
+
+import org.example.trainer.ModelTrainer;
+import smile.classification.Classifier;
+import smile.data.DataFrame;
+import smile.data.formula.Formula;
+
+public class LogisticRegressionTrainer implements ModelTrainer {
+    @Override
+    public Classifier<double[]> train(DataFrame data, Formula formula) {
+        return null;
+    }
+
+    @Override
+    public Classifier<double[]> hyperparameterSearch(DataFrame data, DataFrame validationData, Formula formula) {
+        return ModelTrainer.super.hyperparameterSearch(data, validationData, formula);
+    }
+}
